08c93550a6287a1485611a05110203a39a11e0b8
AndriiSmolinskyi <awful.braid@gmail.com>
2023-04-27 09:03:11 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,66 @@
+let playList = [
+  {
+    author: "LED ZEPPELIN",
 
+    song: "STAIRWAY TO HEAVEN",
+  },
+
+  {
+    author: "QUEEN",
+
+    song: "BOHEMIAN RHAPSODY",
+  },
+
+  {
+    author: "LYNYRD SKYNYRD",
+
+    song: "FREE BIRD",
+  },
+
+  {
+    author: "DEEP PURPLE",
+
+    song: "SMOKE ON THE WATER",
+  },
+
+  {
+    author: "JIMI HENDRIX",
+
+    song: "ALL ALONG THE WATCHTOWER",
+  },
+
+  {
+    author: "AC/DC",
+
+    song: "BACK IN BLACK",
+  },
+
+  {
+    author: "QUEEN",
+
+    song: "WE WILL ROCK YOU",
+  },
+
+  {
+    author: "METALLICA",
+
+    song: "ENTER SANDMAN",
+  },
+];
+
+const playListElement = document.getElementById("playlist");
+
+for (let index = 0; index < playList.length; index++) {
+  let author = playList[index].author;
+  let song = playList[index].song;
+  const listItem = document.createElement("li");
+  listItem.innerText = `${index}) ${author} - ${song}`;
+  playListElement.append(listItem);
+}
+
+/*
+Створити HTML-сторінку з кнопкою "Відкрити" і модальним вікном. На модальном вікні повинен бути текст і кнопка "Закрити". 
+Спочатку модальне вікно не відображається. При кліку на кнопку "Відкрити" з'являється модальне вікно, на кнопку "Закрити" — зникає.
+*/
+
+const button = createElement("button");
