930446fa3be625a3e24608d68a1eaa2deb395b72
AndriiSmolinskyi <awful.braid@gmail.com>
2023-05-01 19:22:26 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
-
 /*
 Створити HTML-сторінку для відображення/редагування тексту. При відкритті сторінки текст відображається за допомогою тега div. 
 При натисканні Ctrl + E, замість div з'являється textarea з тим же текстом, який тепер можна редагувати. При натисканні Ctrl + S, 
 замість textarea з'являється div з уже зміненим текстом. Не забудь вимкнути поведінку за замовчуванням для цих поєднань клавіш.
 */
+
+const divBlock = document.querySelector(".text-block");
+const textAreaBlock = document.createElement("textarea");
+const container = document.querySelector(".container");
+textAreaBlock.id = "textarea";
+textAreaBlock.style.backgroundColor = "red";
+textAreaBlock.textContent = "Це новий блок textarea!";
+container.append(textAreaBlock);
+
+let text = divBlock.innerText;
+document.addEventListener("keydown", (event) => {
+  if (event.ctrlKey && event.key === "y") {
+    textAreaBlock.style.display = "block";
+    textAreaBlock.innerText = text;
+    divBlock.style.display = "none";
+  }
+  if (event.ctrlKey && event.key === "i") {
+    text = textAreaBlock.value; 
+    divBlock.innerText = text;
+    textAreaBlock.style.display = "none";
+    divBlock.style.display = "block";
+  }
+});
+
+/*
+Створити HTML-сторінку з великою таблицею. При кліку на заголовок стовпця, необхідно відсортувати дані цього стовпця.
+, Врахуй, що числові значення повинні сортуватися як числа, а не як рядки.
+*/
+
+
 
 
