c73017f5fdbe2018e108522540874e1a65f2636b
AndriiSmolinskyi <awful.braid@gmail.com>
2023-05-10 15:09:32 +0300

    1

--- js/script.js
+++ js/script.js
@@ -7,34 +7,18 @@ $(".first").slick({
   adaptiveHeight: true
 });
 
+// $(".second").slick({
+//   dots: false,
+//   infinite: true,
+//   speed: 500,
+//   slidesToShow: 5,
+//   slidesToScroll: 1,
+// });
+
 $(".second").slick({
-  dots: true,
+  dots: false,
   infinite: true,
   speed: 500,
   slidesToShow: 5,
   slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
 });
