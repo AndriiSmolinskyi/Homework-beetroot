6ab2daa0dfcf9b063d74a4388f8c6b821a4b33ee
AndriiSmolinskyi <awful.braid@gmail.com>
2023-05-11 12:13:48 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
-
 $(".first-slider").slick({
-   dots: true,
-   infinite: true,
-   speed: 500,
-   slidesToShow: 1,
-   slidesToScroll: 1
- });
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  vertical: true,
+  verticalSwiping: true,
+});
+
+$('a[href^="#"]').on("click", function (event) {
+  var target = $(this.getAttribute("href"));
+  if (target.length) {
+    event.preventDefault();
+    $("html, body").stop().animate(
+      {
+        scrollTop: target.offset().top,
+      },
+      1000
+    );
+  }
+});
