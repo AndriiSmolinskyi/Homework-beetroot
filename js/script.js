557b27a687b1d2ba9895d7a3fa437d472bef3634
AndriiSmolinskyi <awful.braid@gmail.com>
2023-05-11 15:24:17 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-$(".first-slider").slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  vertical: true,
-  verticalSwiping: true,
-});
 
 $('a[href^="#"]').on("click", function (event) {
   var target = $(this.getAttribute("href"));
@@ -20,3 +11,54 @@ $('a[href^="#"]').on("click", function (event) {
     );
   }
 });
+
+const dot1 = document.querySelector(".dot-1");
+const dot2 = document.querySelector(".dot-2");
+const dot3 = document.querySelector(".dot-3");
+const dot4 = document.querySelector(".dot-4");
+const dot5 = document.querySelector(".dot-5");
+const headerBg = document.querySelector(".bg");
+
+dot1.addEventListener('click', (event) =>{
+  dot1.style.backgroundColor = 'white';
+  dot2.style.backgroundColor = 'transparent';
+  dot3.style.backgroundColor = 'transparent';
+  dot4.style.backgroundColor = 'transparent';
+  dot5.style.backgroundColor = 'transparent';
+  headerBg.style.backgroundImage = 'url("/img/tu-tram-pham-202263\ 11234.png"), linear-gradient(243.43deg, #7E5AFF 16.9%, #55B7FF 83.27%)';
+});
+dot2.addEventListener('click', (event) =>{
+  dot1.style.backgroundColor = 'transparent';
+  dot2.style.backgroundColor = 'white';
+  dot3.style.backgroundColor = 'transparent';
+  dot4.style.backgroundColor = 'transparent';
+  dot5.style.backgroundColor = 'transparent';
+  headerBg.style.backgroundImage = 'url("/img/tu-tram-pham-202263\ 11234.png"), linear-gradient(90deg, rgba(235, 231, 105, 1) 0%, rgba(226, 89, 225, 1) 80%)';
+});
+dot3.addEventListener('click', (event) =>{
+  dot1.style.backgroundColor = 'transparent';
+  dot2.style.backgroundColor = 'transparent';
+  dot3.style.backgroundColor = 'white';
+  dot4.style.backgroundColor = 'transparent';
+  dot5.style.backgroundColor = 'transparent';
+  headerBg.style.backgroundImage ='url("/img/tu-tram-pham-202263\ 11234.png"), linear-gradient(90deg, rgba(217, 233, 99, 1) 8%, rgba(89, 170, 226, 1) 70%)';
+});
+dot4.addEventListener('click', (event) =>{
+  dot1.style.backgroundColor = 'transparent';
+  dot2.style.backgroundColor = 'transparent';
+  dot3.style.backgroundColor = 'transparent';
+  dot4.style.backgroundColor = 'white';
+  dot5.style.backgroundColor = 'transparent';
+  headerBg.style.backgroundImage = 'url("/img/tu-tram-pham-202263\ 11234.png"), linear-gradient(90deg, rgba(99, 233, 119, 1) 13%, rgba(89, 177, 226, 1) 54%)';
+});
+dot5.addEventListener('click', (event) =>{
+  dot1.style.backgroundColor = 'transparent';
+  dot2.style.backgroundColor = 'transparent';
+  dot3.style.backgroundColor = 'transparent';
+  dot4.style.backgroundColor = 'transparent';
+  dot5.style.backgroundColor = 'white';
+  headerBg.style.backgroundImage = 'url("/img/tu-tram-pham-202263\ 11234.png"), linear-gradient(90deg, rgba(241, 237, 94, 1) 25%, rgba(89, 177, 226, 1) 74%)';
+});
+
+
+
