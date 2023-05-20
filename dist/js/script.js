912810182b28c6ca6163ec2eb41104d250a8aa01
AndriiSmolinskyi <awful.braid@gmail.com>
2023-05-20 11:26:45 +0300

    1

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,68 +1,113 @@
-class User {
-  // get name() {
-  //   return `${this.first} ${this.last}`;
-  // }
+// class User {
+//   // get name() {
+//   //   return `${this.first} ${this.last}`;
+//   // }
 
-  // set name(first, last){
-  //    this.first = first;
-  //    this.last = last;
-  // }
+//   // set name(first, last){
+//   //    this.first = first;
+//   //    this.last = last;
+//   // }
 
-  constructor(first, last, age) {
-    this.first = first;
-    this.last = last;
-    this.age = age;
-  }
+//   constructor(first, last, age) {
+//     this.first = first;
+//     this.last = last;
+//     this.age = age;
+//   }
 
-  hello() {
-    console.log(`${this.name} hello`);
-  }
-}
+//   hello() {
+//     console.log(`${this.name} hello`);
+//   }
+// }
 
-const someUser = new User("David", "Somelast", 22);
-const someUse = new User("ivan", "Somelast", 24);
+// const someUser = new User("David", "Somelast", 22);
+// const someUse = new User("ivan", "Somelast", 24);
 
-console.log(someUser);
-console.log(someUse);
-console.log('privet')
+// console.log(someUser);
+// console.log(someUse);
+// console.log('privet')
 
-console.log('hy')
+// console.log('hy')
 
+// class Computer{
 
-class Computer{
+//   constructor(name, gpu, cpu, ram, hdd, screen){
+//     this.name = name;
+//     this.gpu = gpu;
+//     this.cpu = cpu;
+//     this.ram = ram;
+//     this.hdd = hdd;
+//     this.screen = screen;
+//   }
 
-  constructor(name, gpu, cpu, ram, hdd, screen){
+//   get fullInfo(){
+//     return `name: ${this.name}, gpu: ${this.gpu}, cpu: ${this.cpu}, ram: ${this.ram}, hdd: ${this.hdd}, screen ${this.screen}`;
+//   }
+
+//   set fullInfo(fullInfo){
+//     [this.name, this.gpu, this.cpu, this.ram, this.hdd, this.screen] = fullInfo.split(' ');
+//   }
+
+//   compOf(){
+//     console.log(`${this.name} off`)
+//   }
+
+//   compOn(){
+//     console.log(`${this.name} on`)
+//   }
+
+// }
+
+// const first = new Computer('1', 'nvidia', 'intel', '16gb', '2tb', '24');
+// const second = new Computer('2', 'nvidia', 'amd', '24gb', '1tb', '27');
+// const third= new Computer('3', 'amd', 'amd', '16gb', '1.5tb', '24');
+// console.log(first.fullInfo);
+// console.log(second.fullInfo);
+// console.log(third.fullInfo);
+// first.fullInfo = '1, amd, intel, 16gb, 2tb, 24';
+// console.log(first.fullInfo);
+
+class Animal {
+  legs;
+  color;
+  wight;
+  constructor(name, color, wight, legs) {
     this.name = name;
-    this.gpu = gpu;
-    this.cpu = cpu;
-    this.ram = ram;
-    this.hdd = hdd;
-    this.screen = screen;
+    this.legs = 4;
+    this.color = color;
+    this.wight = wight;
   }
-
-  get fullInfo(){
-    return `name: ${this.name}, gpu: ${this.gpu}, cpu: ${this.cpu}, ram: ${this.ram}, hdd: ${this.hdd}, screen ${this.screen}`;
+  say() {
+    throw new Error("Not implemented");
   }
-
-  set fullInfo(fullInfo){
-    [this.name, this.gpu, this.cpu, this.ram, this.hdd, this.screen] = fullInfo.split(' ');
+  eat() {
+    console.log("eating");
   }
-
-  compOf(){
-    console.log(`${this.name} off`)
-  }
-
-  compOn(){
-    console.log(`${this.name} on`)
-  }
-
 }
 
-const first = new Computer('1', 'nvidia', 'intel', '16gb', '2tb', '24');
-const second = new Computer('2', 'nvidia', 'amd', '24gb', '1tb', '27');
-const third= new Computer('3', 'amd', 'amd', '16gb', '1.5tb', '24');
-console.log(first.fullInfo);
-console.log(second.fullInfo);
-console.log(third.fullInfo);
-first.fullInfo = '1, amd, intel, 16gb, 2tb, 24';
-console.log(first.fullInfo);
+class Dog extends Animal {
+  #someDogProperty;
+  constructor(name, color, wight, someDogProperty) {
+    super(name, color, wight);
+    this.#someDogProperty = someDogProperty;
+  }
+  say() {
+    console.log("Bark");
+  }
+}
+class Cat extends Animal {
+  someCatProperty;
+
+  constructor(someCatProperty, name, color, wight) {
+    super(name, color, wight);
+    this.someCatProperty = someCatProperty;
+  }
+
+  say() {
+    console.log("Meow");
+  }
+}
+
+const dog = new Dog("Barsik", "Red", 20, "some dog");
+const cat = new Cat("some cat", "Mushka", "Green", 5);
+console.log(dog);
+console.log(cat);
