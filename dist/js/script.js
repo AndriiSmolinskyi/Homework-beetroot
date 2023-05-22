f83a3be30c55f189ec1b26ec00dd7e1360df5532
AndriiSmolinskyi <awful.braid@gmail.com>
2023-05-22 19:17:25 +0300

    1

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,43 +9,40 @@ get-властивість, яке повертає діаметр кола;
 Продемонструй роботу властивостей і методів.
 */
 
-class Circle{
-   constructor(attitude, radius){
-      this.attitude = attitude;
-      this.radius = radius;
-   }
+class Circle {
+  constructor(attitude, radius) {
+    this.attitude = attitude;
+    this.radius = radius;
+  }
 
-   get _Radius(){
-      return `Cicrle-number: ${this.attitude}, radius: ${this.radius}`; 
-   }
+  get _Radius() {
+    return `Cicrle-number: ${this.attitude}, radius: ${this.radius}`;
+  }
 
-   get Diameter(){
-      return `diametr = ${Number(this.radius)*2}`;
-   }
+  get Diameter() {
+    return `diametr = ${Number(this.radius) * 2}`;
+  }
 
-   set _Radius(_Radius){
-      [this.attitude, this.radius] = _Radius.split(' ');
-   }
+  set _Radius(_Radius) {
+    [this.attitude, this.radius] = _Radius.split(" ");
+  }
 
-   circleArea(){
-      return Math.PI * (this.radius*this.radius);
-   }
+  circleArea() {
+    return Math.PI * (this.radius * this.radius);
+  }
 
-   circleLength(){
-      return 2*Math.PI *this.radius;
-   }
-
-   
+  circleLength() {
+    return 2 * Math.PI * this.radius;
+  }
 }
 
 const firstCircle = new Circle(1, 5);
 console.log(firstCircle);
-firstCircle._Radius = '1, 6';
+firstCircle._Radius = "1, 6";
 console.log(firstCircle);
-console.log(firstCircle.Diameter)
+console.log(firstCircle.Diameter);
 console.log(firstCircle.circleArea());
 console.log(firstCircle.circleLength());
-
 
 /*
 2) Реалізуй клас, що описує канцелярський маркер. У класі повинні бути такі компоненти:
@@ -56,115 +53,110 @@ console.log(firstCircle.circleLength());
 Реалізуй клас, що описує маркер, який можна перезаправляти. Успадкуй цей клас від простого маркера й додай метод для заправки.
 */
 
+class Marker {
+  constructor(order, color, precent) {
+    this.order = order;
+    this.color = color;
+    this.precent = precent;
+  }
 
-class Marker{
+  get markInfo() {
+    return this.order, this.color, this.precent;
+  }
 
-   constructor(order, color, precent){
-      this.order = order;
-      this.color = color;
-      this.precent = precent;
-   }
+  set markInfo(markInfo) {
+    [this.order, this.color, this.precent] = markInfo.split(" ");
+  }
 
-   get markInfo(){
-      return this.order, this.color, this.precent;
-   }
+  markInfoWrite() {
+    const bodyJs = document.querySelector("body");
+    const marketText = document.createElement("span");
+    marketText.innerText = `Це ${this.order}ий маркер, його колір ${this.color} та він повний на ${this.precent}%!\n`;
+    marketText.style.color = this.color;
 
-   set markInfo(markInfo){
-      [this.order, this.color, this.precent] = markInfo.split(' ');
-   }
-
-   markInfoWrite() {
-      const bodyJs = document.querySelector('body');
-      const marketText = document.createElement('span');
-      marketText.innerText = `Це ${this.order}ий маркер, його колір ${this.color} та він повний на ${this.precent}%!\n`;
-      marketText.style.color = this.color;
-      
-      for (let index = this.precent; index !== 0; index -= 0.5) {
-        const clonedMarketText = marketText.cloneNode(true);
-        bodyJs.appendChild(clonedMarketText);
-      }
+    for (let index = this.precent; index !== 0; index -= 0.5) {
+      const clonedMarketText = marketText.cloneNode(true);
+      bodyJs.appendChild(clonedMarketText);
     }
+  }
 }
 
-const firstMarker = new Marker(1, 'blue', 10);
+const firstMarker = new Marker(1, "blue", 10);
 console.log(firstMarker);
 firstMarker.markInfoWrite();
-firstMarker.markInfo = '1 red 2';
+firstMarker.markInfo = "1 red 2";
 console.log(firstMarker);
 firstMarker.markInfoWrite();
 
-class RefillableMarker extends Marker{
+class RefillableMarker extends Marker {
+  // constructor(order, color, precent){
+  //    super(order, color, precent);
+  // }
 
-   // constructor(order, color, precent){
-   //    super(order, color, precent);
-   // }
-
-   refuel(fuelPrompt){
-      // const fuelPrompt = prompt('Введіть до 100');
-      this.precent = fuelPrompt;
-   }
+  refuel(fuelPrompt) {
+    // const fuelPrompt = prompt('Введіть до 100');
+    this.precent = fuelPrompt;
+  }
 }
 
-const secondMarker = new RefillableMarker(2, 'grey', 2);
+const secondMarker = new RefillableMarker(2, "grey", 2);
 console.log(secondMarker);
 secondMarker.refuel(5);
 secondMarker.markInfoWrite();
 
 // Реалізуй клас Employee, що описує працівника, і створи масив працівників банку.
-// Реалізуй клас EmpTable для генерації HTML-коду таблиці зі списком працівників банку. 
+// Реалізуй клас EmpTable для генерації HTML-коду таблиці зі списком працівників банку.
 // Масив працівників необхідно передавати через конструктор, а отримувати HTML-код за допомогою методу getHtml ().
 // Створи об’єкт класу EmpTable і виведи на екран результат роботи методу getHtml ().
 
-
-class Employee{
-
-   constructor(name, position, salary){
-      this.name = name;
-      this.position = position;
-      this.salary = salary;
-   }
+class Employee {
+  constructor(name, position, salary) {
+    this.name = name;
+    this.position = position;
+    this.salary = salary;
+  }
 }
 
-class EmpTable{
-   constructor(workers){
-      this.workers = workers;
-   }
+class EmpTable {
+  constructor(workers) {
+    this.workers = workers;
+  }
 
-   getHTML(){
-      const table = document.createElement('table');
-      const tr = document.createElement('tr');
-      const thName = document.createElement('th');
-      thName.innerText = 'name';
-      table.appendChild(thName);
-      const thPos = document.createElement('th');
-      thPos.innerText = 'position';
-      table.appendChild(thPos);
-      const thSalary = document.createElement('th');
-      thSalary.innerText = 'salary';
-      table.appendChild(thSalary);
-      const td = document.createElement('td')
-      for (let index = 0; index < workers.length; index++) {
-         const trCopy = tr.cloneNode(true);
-         const tdCopy1 = td.cloneNode(true);
-         const tdCopy2 = td.cloneNode(true);
-         const tdCopy3 = td.cloneNode(true);
-         const workersArray = workers[index];
-         tdCopy1.textContent = workersArray.name;
-         tdCopy2.textContent = workersArray.position;
-         tdCopy3.textContent = workersArray.salary;
-         trCopy.appendChild(tdCopy1);
-         trCopy.appendChild(tdCopy2);
-         trCopy.appendChild(tdCopy3);
-         table.appendChild(trCopy);
-      }
-      document.body.appendChild(table)
-   }
+  getHTML() {
+    const table = document.createElement("table");
+    const tr = document.createElement("tr");
+    const thName = document.createElement("th");
+    thName.innerText = "name";
+    table.appendChild(thName);
+    const thPos = document.createElement("th");
+    thPos.innerText = "position";
+    table.appendChild(thPos);
+    const thSalary = document.createElement("th");
+    thSalary.innerText = "salary";
+    table.appendChild(thSalary);
+    const td = document.createElement("td");
+    for (let index = 0; index < workers.length; index++) {
+      const trCopy = tr.cloneNode(true);
+      const tdCopy1 = td.cloneNode(true);
+      const tdCopy2 = td.cloneNode(true);
+      const tdCopy3 = td.cloneNode(true);
+      const workersArray = workers[index];
+      tdCopy1.textContent = workersArray.name;
+      tdCopy2.textContent = workersArray.position;
+      tdCopy3.textContent = workersArray.salary;
+      trCopy.appendChild(tdCopy1);
+      trCopy.appendChild(tdCopy2);
+      trCopy.appendChild(tdCopy3);
+      table.appendChild(trCopy);
+    }
+    document.body.appendChild(table);
+  }
 }
 
 const workers = [
-   new Employee('John Doe', 'Manager', 17000),
-   new Employee('Jane Smith', 'Analyst', 17500),
-   new Employee('Mike Johnson', 'Developer', 24000),
+  new Employee("John Doe", "Manager", 17000),
+  new Employee("Jane Smith", "Analyst", 17500),
+  new Employee("Mike Johnson", "Developer", 24000),
 ];
 
 console.log(workers);
