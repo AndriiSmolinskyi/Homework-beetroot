f5b3a84ebb8228aed92ba376b789343e034b8984
AndriiSmolinskyi <awful.braid@gmail.com>
2023-07-11 23:22:57 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 
+// document.cookie = "hello=world";
+// document.cookie = "test=123";
+// console.log(document.cookie);
+
+// document.cookie = "test=000";
+// console.log(document.cookie);
+
+// document.cookie = "color=green; max-age=30; path=/;";
+// console.log(document.cookie);
+
+
+
+//encodeURIComponent
+
+// document.cookie = "login=" +encodeURIComponent("super user");
+// alert(document.cookie);
+
+
+//web storage api
+
+
